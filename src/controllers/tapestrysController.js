b0d2aa5e61b2ae60582f80221ff5208c00c818bd
Kristian Atanasov <124519664+atanasov23@users.noreply.github.com>
2023-06-24 13:30:49 +0300

    Creating a form to add

--- src/controllers/tapestrysController.js
+++ src/controllers/tapestrysController.js
@@ -11,4 +11,14 @@ router.get('/orders', (req, res) => {
     res.render('tapestry-main/orders');
 });
 
+router.get('/adding', (req, res) => {
+
+    res.render('tapestry-main/adding');
+});
+
+router.post('/adding', (req, res) => {
+
+    console.log(req.body);
+});
+
 module.exports = router;
